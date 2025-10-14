978dffe5d107ae5a41fd848192b1102b70f5cef5
Ono <ondraFaiman@atlas.cz>
2025-10-14 22:06:35 +0200

    Add wrapper for the control of the console

---
+++ ConsoleWrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+./ItalianLock.sh
+prog_exit=$?
+cd - &> \dev\null
+
+# Close terminal only if interrupted
+if [[ $prog_exit -ne 0 ]]; then
+    exit
+fi
+export ITALIAN_OFF=1
+exec bash
